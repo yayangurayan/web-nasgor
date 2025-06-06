78570b6f9dfe3f7a4cc23132a779ac1e3e4889ab
yayangurayan <andriandareen@gmail.com>
2025-06-06 11:59:08 +0700

    Delete next-sitemap.config.js

--- next-sitemap.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    siteUrl: 'https://nasgorburina.vercel.app',
-    generateRobotsTxt: true,
-    sitemapSize: 7000
-}
--- next.config.js
+++
@@ -1,5 +0,0 @@
-// next.config.js
-module.exports = {
-  reactStrictMode: true,
-  // Tambahkan konfigurasi lain jika perlu
-};
